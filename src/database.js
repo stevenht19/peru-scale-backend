0074dfae947a82691b79b2b4e4a89e4827ef6548
Matias <gbjosematias@gmail.com>
2024-02-02 00:35:09 -0500

    Validaciones registrarse

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const pool = createPool({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'admin',
+    password: '123456',
     database: 'peruscale'
 });
 
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -14,9 +14,9 @@ router.get('/login', (req, res) => {
 router.get('/account', verifyToken, async (req, res) => {
   try {
     const [user] = await pool.query('SELECT * FROM usuarios WHERE id = ?', [req.user_id]);
-    return res.json({ user: user[0] }) 
+    return res.json({ user: user[0] })
   } catch (err) {
-    return res.json({ message: err }) 
+    return res.json({ message: err })
   }
 })
 
@@ -32,24 +32,61 @@ router.post('/register', async (req, res) => {
   } = req.body;
 
   try {
-    const [existingUser] = await pool.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
-    
-    if (existingUser.length > 0) {
-      return res.status(400).json({ error: true, message: 'El usuario ya existe.' });
+    // Verificar si el correo o el teléfono o dni ya existen en la base de datos wasaaaa :V
+    const [existingEmail] = await pool.query('SELECT * FROM usuarios WHERE correo = ?', [correo]);
+    const [existingPhone] = await pool.query('SELECT * FROM usuarios WHERE telefono = ?', [telefono]);
+    const [existingDNI] = await pool.query('SELECT * FROM usuarios WHERE dni = ?', [dni]);
+
+    //El correo debe incluir el caracter arroba
+    if (!correo.includes('@')) {
+      return res.status(400).json({ error: true, message: 'El correo debe contener el símbolo "@".' });
     }
-        
-    const user = await pool
-      .query('INSERT INTO usuarios (correo, password, nombres, apellidos, direccion, telefono, dni) VALUES (?, ?, ?, ?, ?, ?, ?)', [correo, password, nombres, apellidos, direccion, telefono, dni]);
-    
+
+    // Validar que el teléfono tenga exactamente 9 dígitos y sea numérico
+    if (!/^\d{9}$/.test(telefono)) {
+      return res.status(400).json({ error: true, message: 'El teléfono debe contener exactamente 9 dígitos y ser numérico.' });
+    }
+
+    // Validar que el DNI tenga exactamente 8 dígitos y sea numérico
+    if (!/^\d{8}$/.test(dni)) {
+      return res.status(400).json({ error: true, message: 'El DNI debe contener exactamente 8 dígitos y ser numérico.' });
+    }
+
+
+    if (existingEmail.length > 0) {
+      return res.status(400).json({ error: true, message: 'El correo ya está registrado.' });
+    }
+
+    if (existingPhone.length > 0) {
+      return res.status(400).json({ error: true, message: 'El teléfono ya está registrado.' });
+    }
+
+    if (existingDNI.length > 0) {
+      return res.status(400).json({ error: true, message: 'El DNI ya está registrado.' });
+    }
+
+    // Si no hay errores, proceder con la inserción
+    const user = await pool.query('INSERT INTO usuarios (correo, password, nombres, apellidos, direccion, telefono, dni) VALUES (?, ?, ?, ?, ?, ?, ?)', [correo, password, nombres, apellidos, direccion, telefono, dni]);
+
     const token = jwt.sign({ id: user[0].insertId }, 'TOKEN_KEY', {
       expiresIn: 2000
-    })
+    });
 
-    res.
-      status(201)
-      .json({ user: user[0].insertId, token })
+    res.status(201).json({ user: user[0].insertId, token });
 
   } catch (err) {
+    // Manejar errores especificos, por ejemplo, duplicidad de clave única
+    if (err.code === 'ER_DUP_ENTRY') {
+      if (err.message.includes('correo')) {
+        return res.status(400).json({ error: true, message: 'El correo ya está registrado.' });
+      } else if (err.message.includes('telefono')) {
+        return res.status(400).json({ error: true, message: 'El teléfono ya está registrado.' });
+      } else if (err.message.includes('dni')) {
+        return res.status(400).json({ error: true, message: 'El DNI ya está registrado.' });
+      }
+    }
+
+    // Otros errores
     res.status(500).json({ error: true, message: 'Error interno del servidor.' });
   }
 });
@@ -60,7 +97,7 @@ router.post('/login', async (req, res) => {
     const { correo, password } = req.body;
     const [user] = await pool.query('SELECT * FROM usuarios WHERE correo = ? AND password = ?', [correo, password]);
 
-    
+
     if (user.length) {
       // acceso exitoso
       const token = jwt.sign({ id: user[0].id }, 'TOKEN_KEY', {
@@ -80,11 +117,11 @@ router.post('/recover', verifyToken, async (req, res) => {
   try {
     const { password } = req.body
     await pool.query('UPDATE usuarios SET password = ? WHERE id = ?', [password, req.user_id])
-  
+
     return res.status(200).json({
       message: 'Password Actualizado Correctamente'
     })
-  } catch(_) {
+  } catch (_) {
     return res.status(500).json({
       error: true,
       message: 'Ocurrio un error'
@@ -118,7 +155,7 @@ router.post('/recover-password', async (req, res) => {
     const token = jwt.sign({ id: user[0].id }, 'TOKEN_KEY', {
       expiresIn: 2000
     })
-    
+
     sendEmail(email, `
       Para modificar tu contraseña haz click en este link: <br />
       <a href=http://localhost:5173/recover?token=${token}>
@@ -130,7 +167,7 @@ router.post('/recover-password', async (req, res) => {
       message: 'Email sent successfully'
     })
 
-  } catch(e) {
+  } catch (e) {
     return res.status(404).json({
       error: true,
       message: e.message
